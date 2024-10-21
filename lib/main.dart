dcf5c5165264b75859ff17ccf1db87b77e58f06f
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-10-21 17:46:39 +0700

    Refactor main.dart to update home page to profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:y_mart/pages/cart.dart';
+import 'package:y_mart/pages/forgotpassword.dart';
 import 'package:y_mart/pages/home.dart';
+import 'package:y_mart/pages/homescreen.dart';
+import 'package:y_mart/pages/login.dart';
+import 'package:y_mart/pages/profile.dart';
+import 'package:y_mart/pages/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Poppins'),
-        home: HomePage());
+        home: ProfilePage());
   }
 }
